6e14391f925645de1240544e2494afde2c28f7af
Shadab <Shadab@192.168.1.4>
2018-11-16 01:21:31 +0530

    Updating GoogleTest.java

--- FreeCrmTest/src/com/shadab/integration/GoogleTest.java
+++ FreeCrmTest/src/com/shadab/integration/GoogleTest.java
@@ -2,11 +2,9 @@ package com.shadab.integration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class GoogleTest {
-	@Test
-	public void openBrowser() {
+	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.co.in/");
 	}
