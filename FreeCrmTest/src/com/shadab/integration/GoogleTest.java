a78d54c5f2b8fd480ebed295f978147b796fce60
Shadab <Shadab@192.168.1.4>
2018-11-15 23:22:37 +0530

    1st commit

---
+++ FreeCrmTest/src/com/shadab/integration/GoogleTest.java
@@ -0,0 +1,13 @@
+package com.shadab.integration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+	@Test
+	public void openBrowser() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+	}
+}
